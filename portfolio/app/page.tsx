6a710ced3980fed14d14d8c02b1264f5a0c1c9a0
Hoa Nguyen <hoanguyen.rin@gmail.com>
2025-04-18 13:32:18 +1000

    feat: add about page

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,102 +1,141 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="bg-slate-900 text-slate-50 min-h-screen px-4">
+      <nav className="flex justify-between items-center py-2">
+        <Link href="/" className="font-bold text-green-400">
+          Marcus Tran
+        </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <button>🍔</button>
+      </nav>
+
+      <main className="space-y-4">
+        <section className="text-slate-400 text-sm">
+          <h2 className="text-slate-50 text-lg font-bold mb-2">About me</h2>
+          <p>
+            I have been engaged in professional Web Development for several
+            years. I create Full-Stack websites with a minimalist look and
+            speed. I also focus on website accessibility, website optimization
+            and SEO.
+          </p>
+
+          <p>
+            I like <strong>minimalism</strong>. I stand by the fact that the
+            website should not be full of ads and trackers and completely
+            useless JavaScript. On the other hand, I understand that sometimes a
+            complex website hard to do without a lot of JavaScript. I also use
+            React sometimes.
+          </p>
+
+          <p>
+            But I stand by the fact that{" "}
+            <strong>
+              it is possible to make a good website even without JavaScript
+            </strong>
+            . For example this one. This site is completely JavaScript-free.
+            (except for Cloudflare email protection, so you won&apos;t see my
+            email without JS)
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-slate-50 text-lg font-bold mb-2">Experience</h2>
+          <ul>
+            {[1, 2, 3].map((index) => (
+              <li key={index} className="border-l-2 pl-3 relative">
+                {/* Dot */}
+                <div className="size-3 bg-green-700 rounded-full border-3 absolute top-7 -left-[7px]"></div>
+
+                <Link
+                  href={"/"}
+                  className="text-green-600 underline underline-offset-3 text-sm"
+                >
+                  QPlay.cz
+                </Link>
+                <h3 className="font-semibold text-slate-100">
+                  Full-Stack Web Developer
+                </h3>
+                <p className="text-xs text-slate-400">
+                  October, 2023 - March, 2024
+                </p>
+                <p className="text-sm text-slate-200 mt-2">
+                  Adding features to an already built page. Minor website
+                  modifications. The project was ended in March 2024.
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-slate-50 text-lg font-bold mb-2">Story</h2>
+          <ul>
+            <li className="border-r-2 border-b-2 p-5 relative rounded-br-2xl">
+              <div className="bg-gray-200 size-7 rounded-full text-slate-900 flex justify-center items-center text-sm font-bold absolute top-1/2 -right-3.5 -translate-y-1/2">
+                1
+              </div>
+              <h3 className="mb-2 font-semibold">Beginnings</h3>
+              <p className="text-sm text-slate-400 mb-2 text-justify">
+                Already at the age of 6, I was creating ‘programs’ in Excel and
+                PowerPoint with interactive elements such as buttons.{" "}
+                <span className="text-green-300">Download (Czech)</span>
+              </p>
+              <p className="text-sm text-slate-400 text-justify">
+                In 2018, I started programming for the first time. I started
+                going to a C# programming club. I used WinForms to create
+                console and very simple UI applications.{" "}
+                <span className="text-green-300">Download (Czech)</span>
+              </p>
+            </li>
+            <li className="border-l-2 border-b-2 p-5 relative rounded-bl-2xl rounded-tl-2xl">
+              <div className="bg-gray-200 size-7 rounded-full text-slate-900 flex justify-center items-center text-sm font-bold absolute top-1/2 -left-3.5 -translate-y-1/2">
+                2
+              </div>
+              <h3 className="mb-2 font-semibold">Web development</h3>
+              <p className="text-sm text-slate-400 mb-2 text-justify">
+                Already at the age of 6, I was creating ‘programs’ in Excel and
+                PowerPoint with interactive elements such as buttons.{" "}
+                <span className="text-green-300">Download (Czech)</span>
+              </p>
+            </li>
+            <li className="border-r-2 border-b-2 p-5 relative">
+              <div className="bg-gray-200 size-7 rounded-full text-slate-900 flex justify-center items-center text-sm font-bold absolute top-1/2 -right-3.5 -translate-y-1/2">
+                3
+              </div>
+              <h3 className="mb-2 font-semibold">JavaScript</h3>
+              <p className="text-sm text-slate-400 mb-2 text-justify">
+                Already at the age of 6, I was creating ‘programs’ in Excel and
+                PowerPoint with interactive elements such as buttons.{" "}
+                <span className="text-green-300">Download (Czech)</span>
+              </p>
+              <p className="text-sm text-slate-400 text-justify">
+                In 2018, I started programming for the first time. I started
+                going to a C# programming club. I used WinForms to create
+                console and very simple UI applications.{" "}
+                <span className="text-green-300">Download (Czech)</span>
+              </p>
+            </li>
+            <li className="border-l-2 border-b-2 p-5 relative">
+              <div className="bg-gray-200 size-7 rounded-full text-slate-900 flex justify-center items-center text-sm font-bold absolute top-1/2 -left-3.5 -translate-y-1/2">
+                4
+              </div>
+              <h3 className="mb-2 font-semibold">And more</h3>
+              <p className="text-sm text-slate-400 mb-2 text-justify">
+                Already at the age of 6, I was creating ‘programs’ in Excel and
+                PowerPoint with interactive elements such as buttons.{" "}
+                <span className="text-green-300">Download (Czech)</span>
+              </p>
+            </li>
+          </ul>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="text-center text-xs text-slate-200 border-t mt-7 border-t-slate-600 py-5">
+        <p>© 2025 Jakub Žitník | Created in the Czech Republic</p>
+        <p>
+          Built with love using Hugo . Look at some of my projects on GitHub .
+        </p>
       </footer>
     </div>
   );
